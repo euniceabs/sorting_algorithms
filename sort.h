3bf32f7eaf21f3511ba812f59319793e767f7b08
georgemayne <georgeegyirmayne07@gmail.com>
2023-09-26 22:24:45 -0700

    Additions

--- sort.h
+++ sort.h
@@ -45,11 +45,5 @@ void _swap(int *array, int c, int k);
 void cocktail_sort_list(listint_t **list);
 void swap_nodes(listint_t **node, listint_t **list);
 void counting_sort(int *array, size_t size);
-void merge_sort(int *array, size_t size);
-void heap_sort(int *array, size_t size);
-void radix_sort(int *array, size_t size);
-void bitonic_sort(int *array, size_t size);
-void quick_sort_hoare(int *array, size_t size);
-void sort_deck(deck_node_t **deck);
 
 #endif
